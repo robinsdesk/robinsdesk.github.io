cbd3e84a35b2271989d2e81ca7e870976bfecf1c
Robin Zheng <robinzhengxd@gmail.com>
2019-08-01 23:52:36 -0400

    Codyhouse template

--- js/main.js
+++ js/main.js
@@ -1,90 +1,56 @@
-jQuery(document).ready(function($){
-	var mainHeader = $('.cd-auto-hide-header'),
-		secondaryNavigation = $('.cd-secondary-nav'),
-		//this applies only if secondary nav is below intro section
-		belowNavHeroContent = $('.sub-nav-hero'),
-		headerHeight = mainHeader.height();
+jQuery(document).ready(function(event){
+	var projectsContainer = $('.cd-projects-container'),
+		navigation = $('.cd-primary-nav'),
+		triggerNav = $('.cd-nav-trigger'),
+		logo = $('.cd-logo');
 	
-	//set scrolling variables
-	var scrolling = false,
-		previousTop = 0,
-		currentTop = 0,
-		scrollDelta = 10,
-		scrollOffset = 150;
+	triggerNav.on('click', function(){
+		if( triggerNav.hasClass('project-open') ) {
+			//close project
+			projectsContainer.removeClass('project-open').find('.selected').removeClass('selected').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$(this).children('.cd-project-info').scrollTop(0).removeClass('has-boxshadow');
 
-	mainHeader.on('click', '.nav-trigger', function(event){
-		// open primary navigation on mobile
-		event.preventDefault();
-		mainHeader.toggleClass('nav-open');
-	});
-
-	$(window).on('scroll', function(){
-		if( !scrolling ) {
-			scrolling = true;
-			(!window.requestAnimationFrame)
-				? setTimeout(autoHideHeader, 250)
-				: requestAnimationFrame(autoHideHeader);
+			});
+			triggerNav.add(logo).removeClass('project-open');
+		} else {
+			//trigger navigation visibility
+			triggerNav.add(projectsContainer).add(navigation).toggleClass('nav-open');
 		}
 	});
 
-	$(window).on('resize', function(){
-		headerHeight = mainHeader.height();
+	projectsContainer.on('click', '.single-project', function(){
+		var selectedProject = $(this);
+		if( projectsContainer.hasClass('nav-open') ) {
+			//close navigation
+			triggerNav.add(projectsContainer).add(navigation).removeClass('nav-open');
+		} else {
+			//open project
+			selectedProject.addClass('selected');
+			projectsContainer.add(triggerNav).add(logo).addClass('project-open');
+		}
 	});
 
-	function autoHideHeader() {
-		var currentTop = $(window).scrollTop();
+	projectsContainer.on('click', '.cd-scroll', function(){
+		//scroll down when clicking on the .cd-scroll arrow
+		var visibleProjectContent =  projectsContainer.find('.selected').children('.cd-project-info'),
+			windowHeight = $(window).height();
 
-		( belowNavHeroContent.length > 0 ) 
-			? checkStickyNavigation(currentTop) // secondary navigation below intro
-			: checkSimpleNavigation(currentTop);
+		visibleProjectContent.animate({'scrollTop': windowHeight}, 300); 
+	});
 
-	   	previousTop = currentTop;
+	//add/remove the .has-boxshadow to the project content while scrolling 
+	var scrolling = false;
+	projectsContainer.find('.cd-project-info').on('scroll', function(){
+		if( !scrolling ) {
+		 	(!window.requestAnimationFrame) ? setTimeout(updateProjectContent, 300) : window.requestAnimationFrame(updateProjectContent);
+		 	scrolling = true;
+		}
+	});
+
+	function updateProjectContent() {
+		var visibleProject = projectsContainer.find('.selected').children('.cd-project-info'),
+			scrollTop = visibleProject.scrollTop();
+		( scrollTop > 0 ) ? visibleProject.addClass('has-boxshadow') : visibleProject.removeClass('has-boxshadow');
 		scrolling = false;
-	}
-
-	function checkSimpleNavigation(currentTop) {
-		//there's no secondary nav or secondary nav is below primary nav
-	    if (previousTop - currentTop > scrollDelta) {
-	    	//if scrolling up...
-	    	mainHeader.removeClass('is-hidden');
-	    } else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
-	    	//if scrolling down...
-	    	mainHeader.addClass('is-hidden');
-	    }
-	}
-
-	function checkStickyNavigation(currentTop) {
-		//secondary nav below intro section - sticky secondary nav
-		var secondaryNavOffsetTop = belowNavHeroContent.offset().top - secondaryNavigation.height() - mainHeader.height();
-		
-		if (previousTop >= currentTop ) {
-	    	//if scrolling up... 
-	    	if( currentTop < secondaryNavOffsetTop ) {
-	    		//secondary nav is not fixed
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('fixed slide-up');
-	    		belowNavHeroContent.removeClass('secondary-nav-fixed');
-	    	} else if( previousTop - currentTop > scrollDelta ) {
-	    		//secondary nav is fixed
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.removeClass('slide-up').addClass('fixed'); 
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	}
-	    	
-	    } else {
-	    	//if scrolling down...	
-	 	  	if( currentTop > secondaryNavOffsetTop + scrollOffset ) {
-	 	  		//hide primary nav
-	    		mainHeader.addClass('is-hidden');
-	    		secondaryNavigation.addClass('fixed slide-up');
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	} else if( currentTop > secondaryNavOffsetTop ) {
-	    		//once the secondary nav is fixed, do not hide primary nav if you haven't scrolled more than scrollOffset 
-	    		mainHeader.removeClass('is-hidden');
-	    		secondaryNavigation.addClass('fixed').removeClass('slide-up');
-	    		belowNavHeroContent.addClass('secondary-nav-fixed');
-	    	}
-
-	    }
 	}
 });
